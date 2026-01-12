bceea691230dbb95c74ef0b6c1c392cc594bf210
Vladimir <vladimir@valyakin.ru>
2026-01-12 09:53:26 +0300

    feature/advanced-tasks: Fix Product.Name to be required

--- Advanced/Tasks/LanguagePatterns/Product.cs
+++ Advanced/Tasks/LanguagePatterns/Product.cs
@@ -2,7 +2,7 @@
 
 public class Product
 {
-    public string Name { get; init; }
+    public required string Name { get; init; }
     public decimal Price { get; init; }
     public int Quantity { get; init; }
 }
